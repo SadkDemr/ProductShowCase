233445758646a40b9da61c837af1440eea996e11
SadkDemr <demirsadik1905@gmail.com>
2023-03-09 16:24:25 +0300

    Update
    Update

--- ProductShowCase1/ProductShowCase1/Controllers/ProductController.cs
+++ ProductShowCase1/ProductShowCase1/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -30,6 +31,38 @@ namespace ProductShowCase1.Controllers
         [HttpPost]
         public ActionResult ProductAdd(Product p)
         {
+            if(Request.Files.Count > 0)
+            {
+                string dosyaadi = Path.GetFileName(Request.Files[0].FileName);
+                string uzanti = Path.GetExtension(Request.Files[0].FileName);
+                string yol = "~/Image/" + dosyaadi + uzanti;
+                Request.Files[0].SaveAs(Server.MapPath(yol));
+                p.ProductImage = "/Image/" + dosyaadi + uzanti;
+            }
+            if (Request.Files.Count > 0)
+            {
+                string dosyaadi2 = Path.GetFileName(Request.Files[1].FileName);
+                string uzanti = Path.GetExtension(Request.Files[1].FileName);
+                string yol = "~/Image/" + dosyaadi2 + uzanti;
+                Request.Files[1].SaveAs(Server.MapPath(yol));
+                p.ProductImage2 = "/Image/" + dosyaadi2 + uzanti;
+            }
+            if (Request.Files.Count > 0)
+            {
+                string dosyaadi3 = Path.GetFileName(Request.Files[2].FileName);
+                string uzanti = Path.GetExtension(Request.Files[2].FileName);
+                string yol = "~/Image/" + dosyaadi3 + uzanti;
+                Request.Files[2].SaveAs(Server.MapPath(yol));
+                p.ProductImage3 = "/Image/" + dosyaadi3 + uzanti;
+            }
+            if (Request.Files.Count > 0)
+            {
+                string dosyaadi4 = Path.GetFileName(Request.Files[3].FileName);
+                string uzanti = Path.GetExtension(Request.Files[3].FileName);
+                string yol = "~/Image/" + dosyaadi4 + uzanti;
+                Request.Files[3].SaveAs(Server.MapPath(yol));
+                p.ProductImage4 = "/Image/" + dosyaadi4 + uzanti;
+            }
             c.Products.Add(p);
             c.SaveChanges();
             return RedirectToAction("Index");
@@ -53,8 +86,10 @@ namespace ProductShowCase1.Controllers
             var productValue = c.Products.Find(id);
             return View("ProductBring", productValue);
         }
-        public ActionResult ProductUpdate(Product p)
+        public ActionResult ProductUpdate(Product p, 
+            HttpPostedFileBase ProductImage, HttpPostedFileBase ProductImage2, HttpPostedFileBase ProductImage3, HttpPostedFileBase ProductImage4)
         {
+
             var prot = c.Products.Find(p.ProductID);
             prot.PurchasePrice = p.PurchasePrice;
             prot.Status = p.Status;
@@ -64,7 +99,48 @@ namespace ProductShowCase1.Controllers
             prot.Stock = p.Stock;
             prot.ProductName = p.ProductName;
             prot.ProductFeature = p.ProductFeature;
-            prot.ProductImage = p.ProductImage;
+
+            if (ModelState.IsValid)
+            {
+                if (ProductImage != null && ProductImage.ContentLength > 0)
+                {
+                    string dosyaadi = Path.GetFileName(ProductImage.FileName);
+                    string uzanti = Path.GetExtension(ProductImage.FileName);
+                    string yol = "~/Image/" + dosyaadi + uzanti;
+                    ProductImage.SaveAs(Server.MapPath(yol));
+                    prot.ProductImage = "/Image/" + dosyaadi + uzanti;
+                }
+
+                if (ProductImage2 != null && ProductImage2.ContentLength > 0)
+                {
+                    string dosyaadi2 = Path.GetFileName(ProductImage2.FileName);
+                    string uzanti = Path.GetExtension(ProductImage2.FileName);
+                    string yol = "~/Image/" + dosyaadi2 + uzanti;
+                    ProductImage2.SaveAs(Server.MapPath(yol));
+                    prot.ProductImage2 = "/Image/" + dosyaadi2 + uzanti;
+                }
+
+                if (ProductImage3 != null && ProductImage3.ContentLength > 0)
+                {
+                    string dosyaadi3 = Path.GetFileName(ProductImage3.FileName);
+                    string uzanti = Path.GetExtension(ProductImage3.FileName);
+                    string yol = "~/Image/" + dosyaadi3 + uzanti;
+                    ProductImage3.SaveAs(Server.MapPath(yol));
+                    prot.ProductImage3 = "/Image/" + dosyaadi3 + uzanti;
+                }
+
+                if (ProductImage4 != null && ProductImage4.ContentLength > 0)
+                {
+                    string dosyaadi4 = Path.GetFileName(ProductImage4.FileName);
+                    string uzanti = Path.GetExtension(ProductImage4.FileName);
+                    string yol = "~/Image/" + dosyaadi4 + uzanti;
+                    ProductImage4.SaveAs(Server.MapPath(yol));
+                    prot.ProductImage4 = "/Image/" + dosyaadi4 + uzanti;
+                }
+
+            }
+
+
             c.SaveChanges();
             return RedirectToAction("Index");
 
--- ProductShowCase1/ProductShowCase1/Models/Classes/Product.cs
+++ ProductShowCase1/ProductShowCase1/Models/Classes/Product.cs
@@ -12,18 +12,41 @@ namespace ProductShowCase1.Models.Classes
         [Key]
         public int ProductID { get; set; }
 
+        [Display(Name = "Ürün Adı")]
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string ProductName { get; set; }
 
+        [Display(Name = "Marka")]
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string Brand { get; set; }
+
+        [Display(Name = "Stok")]
         public short Stock { get; set; }
+
+        [Display(Name = "Alış Fiyatı")]
         public decimal PurchasePrice { get; set; }
+
+        [Display(Name = "Satış Fiyatı")]
         public decimal SalePrice { get; set; }
+
+        [Display(Name = "Durumu")]
         public bool Status { get; set; }
+
+        [Display(Name = "Ürün Görseli-1")]
         public string ProductImage { get; set; }
+
+        [Display(Name = "Ürün Görseli-2")]
+        public string ProductImage2 { get; set; }
+
+        [Display(Name = "Ürün Görseli-3")]
+        public string ProductImage3 { get; set; }
+
+        [Display(Name = "Ürün Görseli-4")]
+        public string ProductImage4 { get; set; }
+
+        [Display(Name = "Ürün Özellikleri")]
         public string ProductFeature { get; set; }
         public int CategoryID { get; set; }
         public virtual Category Category { get; set; }
